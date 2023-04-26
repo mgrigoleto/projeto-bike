ad12895d6d9db34fd4847a3b59a297775eb29a3c
mgrigoleto <102479476+mgrigoleto@users.noreply.github.com>
2023-04-26 11:28:14 +0000

    v0.4.0
    - Adicionado o estilo de selecionada para as opções clicadas via JS.
    - Alterado a estrutura do texto de algumas opções para melhorar a responsividade.
    - Adicionado os values nas opções para preparar o JS.

--- script.js
+++ script.js
@@ -20,20 +20,41 @@ perguntas.forEach(pergunta => {// para cada radio
 
 // estilo para as checkbox clicadas (pergunta sobre quais equipamentos são usados)
 const checkboxes = document.querySelectorAll('input[type=checkbox]')// pega os checkbox
-var clicadas = []
 
 checkboxes.forEach(cb => {// para cada checkbox
   cb.addEventListener('click', () => {// deixa clicável, e se clicada
     const labelC = document.querySelector('label[for="'+cb.id+'"')// pega a label da checkbox em questão
-    if(cb.checked && !(clicadas.includes(labelC))){
+    if(cb.checked){
       labelC.classList.add('opt-clicked')
-      clicadas.push(labelC)
-      console.log(clicadas)
-    }else if(cb.checked && (clicadas.includes(labelC))){// para opção de múltipla seleção
+    }else{// para opção de múltipla seleção
       labelC.classList.remove('opt-clicked')
-      let posicao = clicadas.indexOf(labelC)
-      clicadas.splice(posicao, 1)
-      console.log(clicadas)
     }
   })
 })
+
+// aparecer as opções dos equipamentos
+
+
+
+// 
+// 
+// FUNÇÃO
+// 
+// 
+
+// function verificar(){
+//   se resposta = tal
+//   esconde tudo => esconder()
+//   mostrar(nao usa capacete)
+// }
+
+// function esconder(){
+  
+// }
+
+// function mostrar(erro){
+//   se erro = não usar capacete{
+//     mensagem = você não usa capacete
+//   }
+//   return mensagem
+// }
